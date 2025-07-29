c904bb9f63a706c477976d7ca5f7069b2dea5a67
t1ww <95153968+t1ww@users.noreply.github.com>
2025-07-29 15:53:36 +0700

    Update MatchFoundIcon.vue

--- frontend/code-battle/src/components/pvp/MatchFoundIcon.vue
+++ frontend/code-battle/src/components/pvp/MatchFoundIcon.vue
@@ -1,6 +1,7 @@
 <template>
-    <svg viewBox="0 0 24 24" width="64" height="64">
-        <path d="M14 1H6a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h8l5-7V3a2 2 0 0 0-2-2z
-        M14 16H6V3h8v13zm6-7l-3 4V5l3 4z" fill="none" stroke="currentColor" stroke-width="2" />
+    <svg viewBox="0 0 24 24" width="64" height="64" fill="currentColor">
+        <path d="M1 21h4V9H1v12zM23 10c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32
+            c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 
+            1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1z" />
     </svg>
 </template>
