0c0c1bfc234ab40165dfa40e3259d2feed63f095
t1ww <95153968+t1ww@users.noreply.github.com>
2025-08-14 11:10:12 +0700

    forgot styling

--- frontend/code-battle/src/pages/pvp/PvpTypeSelect.vue
+++ frontend/code-battle/src/pages/pvp/PvpTypeSelect.vue
@@ -49,6 +49,65 @@ const get3v3Tooltip = computed(() => {
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #bbb;
+  /* matching the screenshot background */
+}
+
+h1 {
+  margin-bottom: 40px;
+}
+
+.button-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+#pvp1v1-button {
+  margin-right: 6rem;
+}
+
+#pvp3v3-button {
+  margin-left: 6rem;
+}
+
+#pvp-private-button {
+  margin-left: 12rem;
+}
+
+.mode-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  color: inherit;
+  border-radius: .5em;
+  outline: none;
+  width: 12rem;
+  height: 2.5rem;
+  background-color: #ddd;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  transition: transform 0.2s ease, background-color 0.2s ease;
+}
+
+.mode-button:hover {
+  transform: scale(1.05) translateX(5px);
+  background-color: #eee;
+}
+
+.mode-button:active {
+  background-color: #bbb;
+}
+
 .mode-button.disabled {
   pointer-events: none;
   opacity: 0.5;
