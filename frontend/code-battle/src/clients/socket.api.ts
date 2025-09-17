f7c9dc48441684e314a69e919614a496dd39e7cc
t1ww <95153968+t1ww@users.noreply.github.com>
2025-09-17 08:56:28 +0700

    api .env

--- frontend/code-battle/src/clients/socket.api.ts
+++ frontend/code-battle/src/clients/socket.api.ts
@@ -1,7 +1,7 @@
 // frontend\code-battle\src\clients\socket.api.ts
 import { io, Socket } from "socket.io-client"
 
-const URL = "http://localhost:3001" // adjust if different
+const URL = import.meta.env.VITE_SOCKET_URL
 export const socket: Socket = io(URL, {
     autoConnect: false
 })
