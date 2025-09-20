470a1baa7d948ff731fe67cf06491131d14449de
t1ww <95153968+t1ww@users.noreply.github.com>
2025-09-21 02:34:55 +0700

    /api

--- frontend/code-battle/src/clients/crud.api.ts
+++ frontend/code-battle/src/clients/crud.api.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosError } from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   timeout: 10000,
   withCredentials: true,
 });
