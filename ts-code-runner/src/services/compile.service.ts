3b3bf692b05e0db24e7991ada3649e4026ffc2b0
t1ww <95153968+t1ww@users.noreply.github.com>
2025-09-24 15:06:07 +0700

    update g++ cmd Flag
    one for window
    and without for linux (deployed)

--- ts-code-runner/src/services/compile.service.ts
+++ ts-code-runner/src/services/compile.service.ts
@@ -1,9 +1,14 @@
 // ts-code-runner\src\services\compile.service.ts
 import { exec } from "child_process";
 
+const isWindows = process.platform === "win32";
+
 export function compileCode(cppFile: string, exeFile: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    exec(`g++ "${cppFile}" -o "${exeFile}" -mconsole`, (err, _stdout, stderr) => {
+    const flag = isWindows ? "-mconsole" : "";
+    const cmd = `g++ "${cppFile}" -o "${exeFile}" ${flag}`.trim();
+
+    exec(cmd, (err, _stdout, stderr) => {
       if (err) return reject(new Error(stderr || err.message));
       resolve();
     });
