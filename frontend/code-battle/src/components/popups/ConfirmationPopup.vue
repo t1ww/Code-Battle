158afeca6ff7c2080af3a1ed062c96e3c9489271
t1ww <95153968+t1ww@users.noreply.github.com>
2025-10-22 19:11:30 +0700

    Create ConfirmationPopup.vue

---
+++ frontend/code-battle/src/components/popups/ConfirmationPopup.vue
@@ -0,0 +1,25 @@
+<!-- frontend\code-battle\src\components\popups\ConfirmationPopup.vue -->
+<template>
+    <div class="popup-backdrop">
+        <div class="popup-content">
+            <h2>{{ title }}</h2>
+            <p>{{ message }}</p>
+            <button v-on:click="buttonOnYes">{{ yesText }}</button>
+            <button v-on:click="buttonOnNo">{{ noText }}</button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    title: string
+    message: string
+    yesText: string
+    noText: string
+    buttonOnYes: () => void
+    buttonOnNo: () => void
+}>()
+</script>
+
+<!-- Import shared popup CSS -->
+<style src="@/styles/messagePopup.css"></style>
